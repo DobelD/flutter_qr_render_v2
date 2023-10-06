272c580b66605318ab9f3cf3ca81641bfcff87c0
slash code <slashcode.dev@gmail.com>
2023-10-06 20:46:13 +0700

    first

---
+++ android/app/src/main/java/me/ali/flutter_qr_reader/readerView/Orientation.java
@@ -0,0 +1,5 @@
+package me.ali.flutter_qr_reader.readerView;
+
+public enum Orientation {
+  PORTRAIT, LANDSCAPE
+}
---
+++ lib/flutter_qr_render_v2.dart
@@ -0,0 +1,4 @@
+library flutter_qr_render_v2;
+
+export './flutter_qr_scan.dart';
+export './qrcode_reader_view.dart';
